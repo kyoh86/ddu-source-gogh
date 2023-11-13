e4b0b0adcc23ba420eef3fe39b27a3eae88ee34b
kyoh86 <me@kyoh86.dev>
2023-11-14 02:06:40 +0900

    update dependencies

--- denops/@ddu-kinds/gogh_project.ts
+++ denops/@ddu-kinds/gogh_project.ts
@@ -5,8 +5,8 @@ import type {
   PreviewContext,
   Previewer,
 } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { join } from "https://deno.land/std@0.203.0/path/mod.ts";
-import { exists, expandGlob } from "https://deno.land/std@0.203.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.206.0/path/mod.ts";
+import { exists, expandGlob } from "https://deno.land/std@0.206.0/fs/mod.ts";
 import { FileActions } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import { UrlActions } from "https://denopkg.com/4513ECHO/ddu-kind-url@master/denops/@ddu-kinds/url.ts";
 
