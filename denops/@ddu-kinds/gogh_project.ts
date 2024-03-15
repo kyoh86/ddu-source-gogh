6d7e35b9c541ad54eedb39f38765e2a4994b5f6f
GitHub <noreply@github.com>
2024-03-15 09:01:38 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-kinds/gogh_project.ts
+++ denops/@ddu-kinds/gogh_project.ts
@@ -9,8 +9,8 @@ import type {
   PreviewContext,
   Previewer,
 } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
-import { join } from "https://deno.land/std@0.219.1/path/mod.ts";
-import { exists, expandGlob } from "https://deno.land/std@0.219.1/fs/mod.ts";
+import { join } from "https://deno.land/std@0.220.1/path/mod.ts";
+import { exists, expandGlob } from "https://deno.land/std@0.220.1/fs/mod.ts";
 import { FileActions } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import { systemopen } from "https://deno.land/x/systemopen@v0.2.0/mod.ts";
 import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
