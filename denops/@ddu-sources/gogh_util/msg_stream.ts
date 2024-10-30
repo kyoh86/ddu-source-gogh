daa53baacd27981ba853f146d6b5bb2c63362568
kyoh86 <me@kyoh86.dev>
2024-10-31 01:57:26 +0900

    update dependencies

--- denops/@ddu-sources/gogh_util/msg_stream.ts
+++ denops/@ddu-sources/gogh_util/msg_stream.ts
@@ -1,4 +1,4 @@
-import type { Denops } from "jsr:@denops/std@~7.1.0";
+import type { Denops } from "jsr:@denops/std@~7.3.0";
 
 export class EchomsgStream extends WritableStream<string> {
   #cmd = "echomsg";
