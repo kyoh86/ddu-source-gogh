9eea9a70f1bf8e05751b2b26d09c8a05cfa4f7e3
GitHub <noreply@github.com>
2025-02-22 10:54:14 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-kinds/gogh_project.ts
+++ denops/@ddu-kinds/gogh_project.ts
@@ -1,4 +1,4 @@
-import type { Denops } from "jsr:@denops/std@~7.4.0";
+import type { Denops } from "jsr:@denops/std@~7.5.0";
 import { BaseKind } from "jsr:@shougo/ddu-vim@~10.0.0/kind";
 import type {
   DduItem,
